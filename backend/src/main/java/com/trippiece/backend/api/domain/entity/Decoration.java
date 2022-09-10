ad9a93b254dd43dda8e4723a8c559f76c9a73b0a
hseol96 <hseol96@naver.com>
2022-09-11 03:14:16 +0900

    :sparkles: Feat : Decoration Entity, Repository 생성

---
+++ backend/src/main/java/com/trippiece/backend/api/domain/entity/Decoration.java
@@ -0,0 +1,43 @@
+package com.trippiece.backend.api.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "deco")
+public class Decoration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name="sticker_id")
+    @OnDelete(action= OnDeleteAction.CASCADE)
+    private Sticker sticker;
+
+    @ManyToOne
+    @JoinColumn(name="diary_id")
+    @OnDelete(action= OnDeleteAction.CASCADE)
+    private Diary diary;
+
+    @Column(nullable = false)
+    private float x;
+
+    @Column(nullable = false)
+    private float y;
+
+    @Builder
+    public Decoration(Sticker sticker,Diary diary,float x,float y){
+        this.sticker = sticker;
+        this.diary = diary;
+        this.x = x;
+        this.y = y;
+    }
+}
