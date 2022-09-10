03afbfccae0b18e9fc4fc3cdd1db46174840df88
jiwon0297 <https://github.com/jiwon0297>
2022-09-10 14:33:34 +0900

    :sparkles: Feat: Sticker Entity, Repository 및 Place와 관계 설정
    Stiker Entity, Repository를 생성하고 Place Entity와의 관계설정 및 CASCADE 설정 완료. Stiker Entity에서 BaseEntity로 상속받을 id 삭제 예정

---
+++ backend/src/main/java/com/trippiece/backend/api/domain/repository/StickerRepository.java
@@ -0,0 +1,6 @@
+package com.trippiece.backend.api.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StickerRepository extends JpaRepository<StickerRepository, Integer> {
+}
