8d7935177c1b802564d8f22bfa7979ec34b8c51e
SANGBIN IM <97656304+Binzify@users.noreply.github.com>
2022-09-14 12:00:27 +0900

    :sparkles: Feat: 홈화면 전체 레이아웃 구성 완료
    - svg 적용 완료
    - 현재 레이아웃 대충 적용 완료
    - 글꼴 크기, 볼드 수정

--- frontend/src/Pages/Main/MainPage.tsx
+++ frontend/src/Pages/Main/MainPage.tsx
@@ -2,22 +2,23 @@ import React from "react";
 import styled from "@emotion/styled";
 
 import Box from "@mui/material/Box";
-import { ReactComponent as StarplusIcon } from "../../assets/starplus.svg";
+import { pixelToRem } from "../../utils/functions/util";
 
+import { ReactComponent as StarIcon } from "../../assets/starplus.svg";
 import { fetchData } from "../../utils/apis/api";
-
-// 으녈이한테 색상 적용하는거 배우기 - 왜 공식문서대로 해도 모르겠음 ?! 짜증남^.^
 
 const MainBox = styled.div`
   height: 80%;
   box-shadow: 0 4px 4px 2px rgb(0 0 0/25%);
   border-radius: 0 0 20px 20px;
   padding: 30px 0 30px 0;
-  background-color: ${(props) => props.theme.colors.mainGradient};
+  background: ${(props) => props.theme.colors.mainGradient};
 `;
 
-const Notice = styled.div`
-  font-size: ${(props) => props.theme.fontSizes};
+const MiddleTitle = styled.div`
+  font-weight: bold;
+  font-size: ${(props) => props.theme.fontSizes.h3};
+  letter-spacing: -2px;
 `;
 
 const MiddleBoxes = styled.div`
@@ -35,7 +36,7 @@ const InsideLeftBox = styled.div`
   border-left-style: solid;
   border-style: solid dashed solid solid;
   border-width: 3px;
-  border-color: theme.colors.gray300;
+  border-color: ${(props) => props.theme.colors.gray300};
   border-radius: 20px 0 0 20px;
   text-align: center;
 `;
@@ -46,30 +47,50 @@ const InsideRightBox = styled.div`
   height: 400px;
   border-style: solid solid solid hidden;
   border-width: 3px;
-  border-color: gray300;
+  border-color: ${(props) => props.theme.colors.gray300};
   border-radius: 0 20px 20px 0;
   text-align: center;
 `;
 
 const InsideContent = styled.div`
   position: absolute;
-  display: flex;
-  top: 50%;
-  left: 22%;
+  top: 20%;
+  left: 15%;
+  right: 15%;
   justify-content: center;
   align-items: center;
+  color: ${(props) => props.theme.colors.gray300};
 `;
 
-const MiddleTitle = styled.div`
+const InnerTextTitle = styled.div`
+  font-weight: bold;
+  font-size: ${(props) => props.theme.fontSizes.s1};
+`;
+
+const InnerTextBody = styled.div`
+  font-size: ${(props) => props.theme.fontSizes.s3};
+`;
+
+const IconPosition = styled.div`
+  margin: 3.125rem 0;
+`;
+
+const MiddleTitlePosition = styled.div`
   margin: 20px;
   text-align: center;
+`;
+
+const FooterText = styled.div`
+  color: #434343;
+  font-size: ${(props) => props.theme.fontSizes.s2};
+  letter-spacing: 1px;
 `;
 
 // NFT 알려주는 박스 - 추후 위치 기반으로 돌려서 여러개 자동 생성되도록 함
 const GetStickerBox = styled.div`
   width: 201px;
   height: 238px;
-  background-color: gray;
+  background-color: ${(props) => props.theme.colors.gray200};
   border-radius: 15px;
   box-shadow: 0 4px 4px 2px rgb(0 0 0/25%);
   margin: 13px;
@@ -82,23 +103,30 @@ function MainPage() {
         <MiddleBoxes>
           <InsideLeftBox>
             <InsideContent>
-              <p>현재 진행 중인 여행</p>
-              <StarplusIcon width={77} height={77} fill="#dd9c4f" />
-              <p>현재 진행중인 여행이 없습니다.</p>
-              <p>여행을 등록해주세요.</p>
+              <InnerTextTitle>현재 진행 중인 여행</InnerTextTitle>
+              <IconPosition>
+                <StarIcon width="77" height="77" fill="#d4d4d4" />
+              </IconPosition>
+              <InnerTextBody>
+                현재 진행중인 여행이 없습니다. <br />
+                여행을 등록해주세요.
+              </InnerTextBody>
             </InsideContent>
           </InsideLeftBox>
           <InsideRightBox>
-            <InsideContent>
-              <p>티켓 이미지 들어갈 공간</p>
-            </InsideContent>
+            <InsideContent>티켓 이미지 들어갈 공간</InsideContent>
           </InsideRightBox>
         </MiddleBoxes>
       </MainBox>
-      <MiddleTitle>
-        <h3>📍 현재 이 곳에서 스티커를 발급받을 수 있어요!</h3>
-      </MiddleTitle>
+      <MiddleTitlePosition>
+        <MiddleTitle>
+          📍 현재 이 곳에서 스티커를 발급받을 수 있어요!
+        </MiddleTitle>
+      </MiddleTitlePosition>
       <GetStickerBox />
+      <MiddleTitlePosition>
+        <FooterText>Copyright ⓒ2022 여행조각 All rights reserved.</FooterText>
+      </MiddleTitlePosition>
     </Box>
   );
 }
