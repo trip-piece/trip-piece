cba4470a5b4ed61f53fc30e55f3141dd078054f3
jiwon0297 <jiwon0297@naver.com>
2022-09-22 16:03:13 +0900

    :wrench: CorsConfig 재설정

--- backend/src/main/java/com/trippiece/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/trippiece/backend/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.trippiece.backend.config;
 import com.trippiece.backend.interceptor.JwtTokenInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +19,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/user/login/**")
                 .excludePathPatterns("/swagger-resources/**", "/swagger-ui/**", "/v2/api-docs");
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://j7a607.q.ssafy.io");
     }
 }
