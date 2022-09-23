b57059ba06cd6affa3d98fcd59411814de0292ae
hseol96 <hseol96@naver.com>
2022-09-23 15:41:49 +0900

    :lipstick: Style : 관리자페이지 margin 조정 api 급한게 생겨서 일단 push합니다..

--- frontend/src/Pages/Admin/AdminPage.tsx
+++ frontend/src/Pages/Admin/AdminPage.tsx
@@ -1,29 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { Helmet } from "react-helmet-async";
-import { pixelToRem } from "../../utils/functions/util";
-import ColoredRoundButton from "../../components/atoms/ColoredRoundButton";
 import DatePicker from "react-datepicker";
+import Select from "react-select";
+import ColoredRoundButton from "../../components/atoms/ColoredRoundButton";
+import { pixelToRem } from "../../utils/functions/util";
 import "react-datepicker/dist/react-datepicker.css";
 import "../../style/DatePicker.css";
-import Select from "react-select";
 
-const TitleInput = styled.input`
-  border: none;
-  width: 100%;
-  height: ${pixelToRem(40)};
-  padding: 0 1rem;
-  border-radius: 5px;
-  margin: ${pixelToRem(5)};
-`;
-
+// 지역리스트 가져오기
 const options = [
   { value: "chocolate", label: "Chocolate" },
   { value: "strawberry", label: "Strawberry" },
   { value: "vanilla", label: "Vanilla" },
 ];
+
 const Container = styled.section`
   min-height: 100vh;
+  width: 100%;
   border-radius: 30px 30px 0 0;
   padding: 1rem;
   position: relative;
@@ -40,17 +34,23 @@ const Title = styled.h2`
   align-items: center;
 `;
 
-const Margin = styled.div`
-  margin: ${pixelToRem(5)};
+const TitleInput = styled.input`
+  border: none;
+  width: 100%;
+  display: block;
+  height: ${pixelToRem(40)};
+  padding: 0 1rem;
+  border-radius: 5px;
+  margin: ${pixelToRem(10)} 0;
 `;
 
 const InfoBox = styled.div`
   box-shadow: 0 4px 4px 2px rgb(0 0 0/25%);
-  border-radius: 1.25rem 1.25rem 1.25rem 1.25rem;
+  border-radius: 5px;
   padding: 0 0 ${pixelToRem(30)} ${pixelToRem(32)};
-  margin: ${pixelToRem(15)};
-
+  margin: ${pixelToRem(10)} 0;
   height: ${pixelToRem(121)};
+  width: 100%;
   background: ${(props) => props.theme.colors.white};
   display: flex;
 `;
@@ -62,7 +62,15 @@ const ButtonText = styled.div`
   font-color: ${(props) => props.theme.colors.MainDark};
 `;
 
+const Flex = styled.div`
+  display: flex;
+  height: ${pixelToRem(40)};
+`;
+
 function AdminPage() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+
   return (
     <>
       <Helmet>
@@ -78,15 +86,30 @@ function AdminPage() {
         </label>
 
         <TitleInput placeholder="축제 / 지역 명" maxLength={10} />
-        <Margin>
-          <Select options={options} />
-        </Margin>
+
+        <Select options={options} />
 
         <TitleInput placeholder="장소" />
-        <br />
-        <TitleInput placeholder="담당자이메일" />
-        <br />
-
+        <TitleInput placeholder="담당자 이메일" />
+        <Flex>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            dateFormat="yyyy-MM-dd"
+          />
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            dateFormat="yyyy-MM-dd"
+          />
+        </Flex>
         <InfoBox>
           <h5>NFT스티커</h5>
         </InfoBox>
