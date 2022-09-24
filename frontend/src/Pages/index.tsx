a80a4de8a55e3a7306f5159291dabbffa5339d70
유지연 <wldusdl1023@naver.com>
2022-09-24 19:44:29 +0900

    :bug: Fix: Solve Git Conflict
    라우터설정

--- frontend/src/Pages/index.tsx
+++ frontend/src/Pages/index.tsx
@@ -2,4 +2,4 @@ export { default as Main } from "./Main/MainPage";
 export { default as Landing } from "./Landing/LandingPage";
 export { default as TripList } from "./TripList/TripListPage";
 export { default as TripDiaryList } from "./TripDiaryList/TripDiaryListPage";
-export { default as MyPage } from "./MyPage/MySticker";
+export { default as MyPage } from "./MyPage/MyScrap";
