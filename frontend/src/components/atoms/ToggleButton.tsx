4dede7c4abf3dfff26fa006ae7580a1ab009a982
SANGBIN IM <97656304+Binzify@users.noreply.github.com>
2022-09-24 15:23:31 +0900

    :lipstick: Style: 홈/메뉴/발급지역 재작성 및 라우터 연결
    - 홈화면/메뉴/발급지역 파일 재작성 후 라우터 URL 연결

---
+++ frontend/src/components/atoms/ToggleButton.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+import { pixelToRem } from "../../utils/functions/util";
+
+const Background = styled.div`
+  display: inline-block;
+  width: 50%;
+  height: ${pixelToRem(45)};
+  border-radius: ${pixelToRem(20)};
+  background-color: ${(props) => props.theme.colors.gray200};
+  z-index: -10;
+`;
+
+const Toggled = styled.div`
+  position: relative;
+  width: 50%;
+  height: ${pixelToRem(45)};
+  border-radius: ${pixelToRem(20)};
+  background-color: ${(props) => props.theme.colors.mainDark};
+  z-index: 0;
+  left: ${({ toggle }) => (toggle === "off" ? "0" : "50%")};
+  transition: all 400ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+`;
+
+const TextBox = styled.div`
+  position: relative;
+  width: 100%;
+  height: ${pixelToRem(45)};
+  line-height: ${pixelToRem(45)};
+  top: -100%;
+  display: flex;
+  justify-content: space-around;
+  z-index: 10;
+  > Box {
+    > div {
+      color: ${(props) => props.theme.colors.gray0};
+    }
+  }
+`;
+
+export default function ToggleButton(props) {
+  const [toggle, setToggle] = React.useState("off");
+
+  function onClickLeft() {
+    if (props && props.onClickLeft) props.onClickLeft();
+    setToggle("off");
+  }
+  function onClickRight() {
+    if (props && props.onClickRight) props.onClickRight();
+    setToggle("on");
+  }
+
+  return (
+    <Background>
+      <Toggled toggle={toggle} />
+      <TextBox>
+        <Box onClick={onClickLeft}>
+          <p>{props.textLeft}</p>
+        </Box>
+        <Box onClick={onClickRight}>
+          <div>{props.textRight}</div>
+        </Box>
+      </TextBox>
+    </Background>
+  );
+}
