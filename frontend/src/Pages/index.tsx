e637bf8771c8e07f05f3a9a2df2d66dbe864a1bd
유지연 <wldusdl1023@naver.com>
2022-09-27 06:36:13 +0900

    :lipstick: Style: 마이페이지 css, 버튼 navigate, 무한스크롤 구현완 료
    select box는 아직 안했다구함 ㅋㅋ

--- frontend/src/Pages/index.tsx
+++ frontend/src/Pages/index.tsx
@@ -2,6 +2,7 @@ export { default as Main } from "./Main/MainPage";
 export { default as Landing } from "./Landing/LandingPage";
 export { default as TripList } from "./TripList/TripListPage";
 export { default as TripDiaryList } from "./TripDiaryList/TripDiaryListPage";
-export { default as MyPage } from "./MyPage/MyScrap";
+export { default as MyScrap } from "./MyPage/MyScrap";
+export { default as MySticker } from "./MyPage/MySticker";
 export { default as QrReader } from "./QrScan/QrReader";
 export { default as NftResponse } from "./QrScan/NftResponse";
