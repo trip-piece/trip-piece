3ad9ad2caed7be78aad9a2c51d8e8c5cdb441dc3
유지연 <wldusdl1023@naver.com>
2022-09-29 03:26:17 +0900

    :sparkles: Feat: 토큰 리이슈 인터셉터 처리
    테스트안해봄 ㅋ

--- frontend/src/utils/apis/api.ts
+++ frontend/src/utils/apis/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from "axios";
-import { getCookie } from "../cookie";
+import { getCookie, setCookie } from "../cookie";
+import userApis from "./userApis";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: "http://j7a607.q.ssafy.io:8080",
@@ -10,5 +11,34 @@ const axiosInstance: AxiosInstance = axios.create({
     "Access-Control-Allow-Origin": "*",
   },
 });
+
+const getNewAccessToken = (access: string, refresh: string) => {
+  return axiosInstance.patch(userApis.tokenReissue, {
+    headers: { ACCESS_TOKEN: access, REFRESH_TOKEN: refresh },
+  });
+};
+
+axiosInstance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    if (err.response) {
+      if (
+        err.response.status === 401 &&
+        err.response.data?.error === "TokenExpiredException"
+      ) {
+        const response = await getNewAccessToken(
+          getCookie("ACCESS_TOKEN"),
+          getCookie("REFRESH_TOKEN"),
+        );
+        console.log(response);
+        const { accessToken, refreshToken } = response.data;
+        setCookie("accessToken", accessToken);
+        setCookie("refreshToken", refreshToken);
+      }
+    }
+  },
+);
 
 export default axiosInstance;
