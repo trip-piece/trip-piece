af09639b01ceb7e79b3caf52a891857d9359eea0
유지연 <wldusdl1023@naver.com>
2022-09-30 12:45:05 +0900

    :sparkles: Feat: 닉네임수정
    ㅋㅋ

--- frontend/src/Pages/MyPage/Modal.tsx
+++ frontend/src/Pages/MyPage/Modal.tsx
@@ -6,6 +6,12 @@ import styled from "@emotion/styled";
 import { pixelToRem } from "../../utils/functions/util";
 import ColoredRoundButton from "../../components/atoms/ColoredRoundButton";
 import { ReactComponent as PencilIcon } from "../../assets/svgs/pencilIcon.svg";
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { IUserInfo, UserInfoState } from "../../store/atom";
+import { SubmitHandler, useForm } from "react-hook-form";
+import userApis, { Inickname } from "../../utils/apis/userApis";
+import axiosInstance from "../../utils/apis/api";
 // import axiosInstance from "../../utils/apis/api";
 // import userApis, { Inickname } from "../../utils/apis/userApis";
 // import { useRecoilState } from "recoil";
@@ -68,23 +74,29 @@ const InputBox = styled.input`
 `;
 
 export default function NestedModal() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [userInfoState, setUserInfoState] = useRecoilState(UserInfoState);
   // const [userInfo, setUserInfo] = useRecoilState(UserInfoState);
+
   const handleOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
   };
-  // const { handleSubmit } = useForm<Inickname>();
+  const { register, handleSubmit } = useForm<Inickname>();
 
-  // const onSubmit: SubmitHandler<Inickname> = async (data: Inickname) => {
-  //  let response = await axiosInstance.patch(userApis.modifyNickname, data);
+  const onSubmit: SubmitHandler<Inickname> = async (data: Inickname) => {
+    console.log("zz");
 
-  // if (response.status === 200) {
-  // setUserInfo({ nickname: data.nickname });
-  //  }
-  // }
+    const response = await axiosInstance.patch(userApis.modifyNickname, data);
+
+    if (response.status === 200) {
+      const info: IUserInfo = { ...userInfoState, nickname: data.nickname };
+      setUserInfoState(info);
+    }
+    handleClose();
+  };
   return (
     <>
       <Button onClick={handleOpen}>
@@ -99,25 +111,27 @@ export default function NestedModal() {
         <ModifiedNicknameModal>
           <Container>
             <Title>닉네임 수정</Title>
-            <Form>
-              <InputBox placeholder="1 ~ 8자 이내로 입력" />
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <InputBox
+                placeholder="1 ~ 8자 이내로 입력"
+                {...register("nickname")}
+              />
+              <LeftContainer>
+                <ColoredRoundButton
+                  text=" 수정 "
+                  color="mainLight"
+                  type="submit"
+                />
+              </LeftContainer>
+              <RightContainer>
+                <ColoredRoundButton
+                  text="취소"
+                  color="gray400"
+                  type="button"
+                  func={handleClose}
+                />
+              </RightContainer>
             </Form>
-
-            <LeftContainer>
-              <ColoredRoundButton
-                text=" 수정 "
-                color="mainLight"
-                type="submit"
-              />
-            </LeftContainer>
-            <RightContainer>
-              <ColoredRoundButton
-                text="취소"
-                color="gray400"
-                type="button"
-                func={handleClose}
-              />
-            </RightContainer>
           </Container>
         </ModifiedNicknameModal>
       </Modal>
