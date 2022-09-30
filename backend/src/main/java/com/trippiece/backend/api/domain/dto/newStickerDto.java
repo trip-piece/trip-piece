71ccb61c2389cb726cd16f1f57d5b853c637a9a9
jiwon0297 <jiwon0297@naver.com>
2022-09-30 22:53:13 +0900

    :bug: Fix: Place 등록 API 버그 수정

--- backend/src/main/java/com/trippiece/backend/api/domain/dto/newStickerDto.java
+++ backend/src/main/java/com/trippiece/backend/api/domain/dto/newStickerDto.java
@@ -4,22 +4,14 @@ import com.trippiece.backend.api.domain.entity.Sticker;
 import lombok.Getter;
 
 @Getter
-public class StickerDto implements Comparable<StickerDto> {
-    private long id;
+public class newStickerDto {
     private long tokenId;
     private String tokenName;
     private String tokenURL;
 
-    public StickerDto(Sticker sticker) {
-        this.id = sticker.getId();
+    public newStickerDto(Sticker sticker) {
         this.tokenId = sticker.getTokenId();
         this.tokenName = sticker.getTokenName();
         this.tokenURL = sticker.getTokenURL();
     }
-
-    @Override
-    public int compareTo(StickerDto o) {
-        return (int) (this.id-o.getId());
-    }
 }
-
