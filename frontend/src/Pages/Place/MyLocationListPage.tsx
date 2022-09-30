c17169c68622e96c05352184e5eff37b8113e503
jiwon0297 <jiwon0297@naver.com>
2022-09-30 14:19:34 +0900

    :sparkles: Feat: 내 주변 발급 가능 지역 api 연결

--- frontend/src/Pages/Place/MyLocationListPage.tsx
+++ frontend/src/Pages/Place/MyLocationListPage.tsx
@@ -1,16 +1,15 @@
 import styled from "@emotion/styled";
-import { useNavigate, useParams } from "react-router-dom";
-import { MdLocationOn } from "react-icons/md";
+import { MdArrowBack } from "react-icons/md";
 import { Helmet } from "react-helmet-async";
-import { REGIONLIST } from "../../utils/constants/constant";
-import { pixelToRem } from "../../utils/functions/util";
+import { useEffect, useState } from "react";
+import { getLocation, pixelToRem } from "../../utils/functions/util";
 import Container from "../../components/atoms/Container";
-import { IPlace } from "../../utils/interfaces/places.interface";
-import { useState } from "react";
-import { AxiosError, AxiosResponse } from "axios";
-import axiosInstance from "../../utils/apis/api";
 import { placeApis } from "../../utils/apis/placeApis";
-import { useQuery } from "react-query";
+import { MemoInfiniteList } from "../../components/modules/infinite/ParamsInfiniteList";
+import { MemoCard } from "./Card";
+import Skeleton from "./Skeleton";
+import { motion } from "framer-motion";
+import { BiCurrentLocation } from "react-icons/bi";
 
 const TitleGroup = styled.div`
   width: 100%;
@@ -32,7 +31,6 @@ const NearbyMyLocationBtn = styled.button`
   height: ${pixelToRem(30)};
   border-radius: ${pixelToRem(10)};
   background: ${(props) => props.theme.colors.mainDark};
-  padding: ${pixelToRem(3)};
   margin-top: ${pixelToRem(7)};
   color: ${(props) => props.theme.colors.gray0};
   font-size: ${(props) => props.theme.fontSizes.s1};
@@ -55,49 +53,100 @@ const NearbyMyLocationBtn = styled.button`
   }
 `;
 
+const ToggleGroup = styled.div`
+  width: 35%;
+  height: 30px;
+  background-color: ${(props) => props.theme.colors.gray200};
+  text-align: center;
+  margin: auto;
+  border-radius: 15px;
+  margin-bottom: 1.5rem;
+
+  .inactive {
+    width: 50%;
+    height: 100%;
+    border-radius: 15px;
+    background-color: transparent;
+    font-size: ${(props) => props.theme.fontSizes.h5};
+  }
+
+  .active {
+    width: 50%;
+    height: 100%;
+    border-radius: 15px;
+    background-color: ${(props) => props.theme.colors.mainDark};
+    color: ${(props) => props.theme.colors.white};
+    font-size: ${(props) => props.theme.fontSizes.h5};
+  }
+`;
+
 const PlaceList = styled.div``;
 
 function MyLocationListPage() {
-  const [festivals, setFestivals] = useState<IPlace[]>([]);
-  const [spots, setSpots] = useState<IPlace[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const { regionId } = useParams();
-  const navigate = useNavigate();
-  const moveToMyLocation = () => {
-    navigate(`/places/list/mylocation`);
+  const [locationInfo, setLocationInfo] = useState("");
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+
+  const myLocation = async () => {
+    const userLocation: any = await getLocation();
+    setLocationInfo(userLocation.location);
+    setLat(userLocation.latitude);
+    setLng(userLocation.longitude);
   };
 
-  const {
-    isLoading: isLoading1,
-    isSuccess: isSuccess1,
-    data: data1,
-  } = useQuery<AxiosResponse<IPlace[]>, AxiosError>(
-    [`${regionId}-festivals`],
-    () => axiosInstance.get(placeApis.getPlaces(Number(regionId), 0)),
-    {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      refetchOnMount: true,
-    },
-  );
+  const updateLocation = async () => {
+    await myLocation();
+  };
+
+  useEffect(() => {
+    myLocation();
+  }, []);
 
   return (
     <>
       <Helmet>
-        <title>내 주변 지역 | 여행조각</title>
+        <title>이벤트 리스트 | 여행조각</title>
       </Helmet>
       <Container hasPadding>
         <TitleGroup>
-          <h1 className="main">{REGIONLIST[Number(regionId)]}</h1>
-          <h1>현재 발급 가능한 00 곳의 스팟이 있어요 !</h1>
-          <div style={{ width: "100%", textAlign: "right" }}>
-            <NearbyMyLocationBtn onClick={moveToMyLocation}>
-              <MdLocationOn />
-              <p>내 주변</p>
-            </NearbyMyLocationBtn>
+          <div style={{ width: "100%", textAlign: "left" }}>
+            <MdArrowBack size="30" />
+          </div>
+          <h1 className="main">내 주변</h1>
+          <div
+            style={{
+              width: "100%",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              style={{
+                color: "#D35B5B",
+                background: "transparent",
+                width: "7%",
+              }}
+              onClick={updateLocation}
+            >
+              <BiCurrentLocation size="18" />
+            </motion.button>
+            <p>{locationInfo}</p>
           </div>
         </TitleGroup>
-        <PlaceList></PlaceList>
+        <PlaceList>
+          <MemoInfiniteList
+            url={placeApis.getLocationPlaces(lat, lng)}
+            queryKey={["mylocationList"]}
+            CardComponent={MemoCard}
+            SkeletonCardComponent={Skeleton}
+            zeroDataText="NFT 스티커 발급 가능한 스팟이 없습니다."
+            count={1}
+            listName="mylocationList"
+          />
+        </PlaceList>
       </Container>
     </>
   );
