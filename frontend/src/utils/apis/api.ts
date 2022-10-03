0daf1c47d704c9e4cbfad1c9f16c65a8e7ee01e1
유지연 <wldusdl1023@naver.com>
2022-10-03 16:53:03 +0900

    :sparkles: Feat: 새로고침시 유저정보 로그인 안될때도 불러와지는거 수정

--- frontend/src/utils/apis/api.ts
+++ frontend/src/utils/apis/api.ts
@@ -27,44 +27,37 @@ axiosInstance.interceptors.request.use(
   },
 );
 
-// const axiosInstance: AxiosInstance = axios.create({
-//   //baseURL: "http://j7a607.q.ssafy.io:8080",
-//   baseURL: "https://j7a607.q.ssafy.io/api",
-//   headers: {
-//     "Content-Type": "application/json",
-//     ACCESS_TOKEN: getCookie("accessToken"),
-//     "Access-Control-Allow-Origin": "*",
-//   },
-// });
+const getNewAccessToken = (access: string, refresh: string) => {
+  return axiosInstance.patch(userApis.tokenReissue, {
+    headers: { ACCESS_TOKEN: access, REFRESH_TOKEN: refresh },
+  });
+};
 
-// const getNewAccessToken = (access: string, refresh: string) => {
-//   return axiosInstance.patch(userApis.tokenReissue, {
-//     headers: { ACCESS_TOKEN: access, REFRESH_TOKEN: refresh },
-//   });
-// };
+axiosInstance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    console.log("인터셉터 발동");
+    console.log(err.response.status);
+    console.log(err.response);
 
-// axiosInstance.interceptors.response.use(
-//   (res) => {
-//     return res;
-//   },
-//   async (err) => {
-//     console.log("인터셉터 발동");
-//     console.log(err.response.status);
-//     console.log(err.response);
-//     if (err.response) {
-//       if (
-//         err.response.status === 401 &&
-//         err.response.data?.error === "TokenExpiredException"
-//       ) {
-//         const response = await getNewAccessToken(
-//           getCookie("ACCESS_TOKEN"),
-//           getCookie("REFRESH_TOKEN"),
-//         );
-//         console.log(response);
-//         const { accessToken, refreshToken } = response.data;
-//         setCookie("accessToken", accessToken);
-//         setCookie("refreshToken", refreshToken);
-//       }
-//     }
-//   },
-// );
+    const originalConfig = err.config;
+
+    if (err.response) {
+      if (
+        err.response.status === 401 &&
+        err.response.data?.error === "TokenExpiredException"
+      ) {
+        const response = await getNewAccessToken(
+          getCookie("ACCESS_TOKEN"),
+          getCookie("REFRESH_TOKEN"),
+        );
+        console.log(response);
+        const { accessToken, refreshToken } = response.data;
+        setCookie("accessToken", accessToken);
+        setCookie("refreshToken", refreshToken);
+      }
+    }
+  },
+);
