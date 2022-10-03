c4f65327b3b5d0eb66702fe472ca3f93ea12d85e
jiwon0297 <jiwon0297@naver.com>
2022-10-03 18:45:01 +0900

    :lipstick: Style: 애니메이션

--- frontend/src/Pages/MyPage/MySticker.tsx
+++ frontend/src/Pages/MyPage/MySticker.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { motion } from "framer-motion";
 import UserInfo from "./UserInfo";
 import Tap from "./Tap";
 import MyStickerList from "./MyStickerList";
 
 function MySticker() {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.7 }}
+    >
       <Helmet>
         <title>보유 NFT 스티커 | 여행조각</title>
       </Helmet>
@@ -14,7 +20,7 @@ function MySticker() {
       <UserInfo />
       <Tap type={0} />
       <MyStickerList />
-    </>
+    </motion.div>
   );
 }
 
